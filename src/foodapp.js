b63901c0aeb1ef4422c7fdf2726118ea430e62ad
Rikard Legge <rikard.legge@gmail.com>
2017-12-02 16:30:16 +0100

    Include add, remove and generate methods

--- src/foodapp.js
+++ src/foodapp.js
@@ -6,18 +6,51 @@ var config = {
 };
 firebase.initializeApp(config);
 
-firebase.database().ref('/fridge').on('value', function(snapshot) {
-  console.log(snapshot.val());
-});
+const fridgeDb = firebase.database();
+const fridgeRef = fridgeDb.ref('/fridge');
+const now = new Date("2017-12-1");
 
-// let defaultOptions = {
-//   url:'',
-//   method:'POST',
-//   mode: 'cors',
-  // headers: new Headers({
-  //   'Access-Control-Allow-Origin':'*'
-  // }),
-  // body:"needsimage=1&kitchen=butter%2Cbanana%2Ccinnamon%2Capple&focus=&kw=&catname=Dinner%2C&exclude=&start=0",
-// };
-//
-// fetch("http://www.supercook.com/dyn/results", defaultOptions);
+function addIngredient(name, expires) {
+  const ingredientRef = fridgeDb.ref(`/fridge/${name}`);
+  ingredientRef.set({expiresOn: expires});
+}
+
+function removeIngredient(name) {
+  fridgeDb.ref(`/fridge/${name}`).remove();
+}
+
+function generateRecipiesFromIngredients(daysOld) {
+  return new Promise((resolve) => {
+    fridgeRef.once('value').then(async function(snapshot) {
+      const val = snapshot.val();
+      let ingredients = Object.keys(val).filter(key => lessThan(val[key].expiresOn, daysOld));
+      if(ingredients.length  === 0) {
+        ingredients = Object.keys(val);
+      }
+
+      console.log("Using ingredients", ingredients);
+      const result = await getRecipies(ingredients);
+      result.json().then(function(recipies) {
+        resolve(recipies);
+      });
+    });
+  });
+}
+
+function lessThan(dateString, days) {
+  const date = new Date(dateString);
+  // Allow negative times
+  const timeDiff = date.getTime() - now.getTime();
+  return timeDiff < 1000*3600*24*days;
+}
+
+function getRecipies(include) {
+  const exclude = [];
+  const excludeStr = exclude.join(',');
+  const includeStr = include.join(',');
+
+  return fetch("http://www.supercook.com/dyn/results", {
+    method:'POST',
+    body:`needsimage=0&kitchen=${includeStr}&focus=&kw=&catname=Dinner&exclude=${excludeStr}&start=0`,
+  });
+}
