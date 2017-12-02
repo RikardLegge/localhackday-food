46ac39206b47b5c5cc38ed49f9b4b6dc57ae3877
Linnéa <linneateigen@hotmail.com>
2017-12-02 18:26:57 +0100

    Started with the graphic interface!

--- src/foodapp.js
+++ src/foodapp.js
@@ -9,6 +9,25 @@ firebase.initializeApp(config);
 const fridgeDb = firebase.database();
 const fridgeRef = fridgeDb.ref('/fridge');
 const now = new Date("2017-12-1");
+updateUiWhenDbChanges(document.querySelector(".list"));
+
+{
+  const nameText = document.querySelector(".addIngredientName");
+  const dateText = document.querySelector(".addIngredientDate");
+  nameText.onkeydown  = onDown;
+  dateText.onkeydown  = onDown;
+
+  function onDown(event) {
+    if (event.keyCode == 13) {
+      const name = nameText.value;
+      const date = dateText.value;
+      addIngredient(name, date);
+
+      nameText.value = "";
+      dateText.value = "";
+    }
+  }
+}
 
 function addIngredient(name, expires) {
   const ingredientRef = fridgeDb.ref(`/fridge/${name}`);
@@ -17,6 +36,29 @@ function addIngredient(name, expires) {
 
 function removeIngredient(name) {
   fridgeDb.ref(`/fridge/${name}`).remove();
+}
+
+function updateUiWhenDbChanges(list){
+  fridgeRef.on('value', function(snapshot) {
+    const val = snapshot.val();
+
+    let html = "";
+    Object.keys(val).forEach(name => {
+      const expiration = val[name].expiresOn;
+      html += `
+          <li>
+            <div>${name} expires on ${expiration}</div>
+            <button class="btn btn-default" data-id="${name}">Remove</button>
+          </li>`;
+    });
+
+    list.innerHTML = html;
+    list.onclick = function(e) {
+      const name = e.target.dataset.id;
+      removeIngredient(name);
+    }
+
+  })
 }
 
 function generateRecipiesFromIngredients(daysOld) {
