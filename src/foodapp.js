de1211b7ce06a3244e40eeb8ec5270b0d83e0f66
Rikard Legge <rikard.legge@gmail.com>
2017-12-02 15:37:14 +0100

    Rename test to fridge

--- src/foodapp.js
+++ src/foodapp.js
@@ -6,6 +6,18 @@ var config = {
 };
 firebase.initializeApp(config);
 
-firebase.database().ref('/test').on('value', function(snapshot) {
-  console.log(snapshot);
+firebase.database().ref('/fridge').on('value', function(snapshot) {
+  console.log(snapshot.val());
 });
+
+// let defaultOptions = {
+//   url:'',
+//   method:'POST',
+//   mode: 'cors',
+  // headers: new Headers({
+  //   'Access-Control-Allow-Origin':'*'
+  // }),
+  // body:"needsimage=1&kitchen=butter%2Cbanana%2Ccinnamon%2Capple&focus=&kw=&catname=Dinner%2C&exclude=&start=0",
+// };
+//
+// fetch("http://www.supercook.com/dyn/results", defaultOptions);
